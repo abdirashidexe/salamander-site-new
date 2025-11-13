6ab54574a5e42c9912ca166aad71eb760c2dcfb8
blake <HansenBlakestudentgreenriveredu@noreply.github.com>
2025-11-13 14:15:25 -0800

    Added Logo image and updated Header component to use Next.js Image component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
+
 
 export default function Header() {
-  return (
-    <header>
+    return (
+      <header>
+        <>
+            <Image
+              src="/Salamander-Logo.webp"
+              alt="Salamander Logo"
+              className="w-[40px] h-auto block md:hidden"
+              width={300}
+              height={200}
+            />
+            <h1>header</h1>
+        </>
       <h1>My App</h1>
       <nav>
         <Link href="/">Home</Link>
