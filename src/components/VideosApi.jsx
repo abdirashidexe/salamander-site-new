f6ea35e3c495e0db7a66b72e8e0dc1c30c225863
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-12-01 13:42:58 -0800

    outputting the video files on the page

--- src/components/VideosApi.jsx
+++ src/components/VideosApi.jsx
@@ -7,7 +7,10 @@ export default function VideosApi() {
     const [videos, setVideos] = React.useState(null)
     fetch("http://localhost:3000/api/videos")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            setVideos(data)          
+        })
+        
         console.log("Fetching WORKED!")
     return (
         <div>
