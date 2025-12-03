3a5a1c9bc1ba34b54d716330bcdb898087e07b0a
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-12-03 11:22:33 -0800

    thumbnails work!

--- src/components/VideosApi.jsx
+++ src/components/VideosApi.jsx
@@ -1,32 +1,40 @@
 "use client"
 
 import React from "react"
-import Link from "next/link";
 import Image from "next/image";
-import salamanderLogo from "../public/bestSalamanderImage.png";
 
 export default function VideosApi() {
 
     const [videos, setVideos] = React.useState([])
-    fetch("http://localhost:3000/api/videos")
-        .then(res => res.json())
-        .then(data => {
-            setVideos(data.videos)          
-        }, [])
+
+    // 1. Fetch the list of video names
+    React.useEffect(() => {
+      fetch("http://localhost:3000/api/videos")
+        .then((res) => res.json())
+        .then((data) => {
+          setVideos(data.videos);
+        })
+        .catch(err => console.error("Error fetching videos:", err));
+    }, []);
         
-        console.log("Fetching WORKED!")
+    console.log("Fetching WORKED!")
+
     return (
-      <>
-        <div id="video-boxes">
-          {videos.map(file => (
+      <div id="video-boxes">
+
+        {/* 2. Loop through the videos array & make the url link*/}
+        {videos.map(file => {
+          const thumbnailUrl = `http://localhost:3000/api/thumbnail/${file}`;
+          
+          return (
             <div key={file} className="video-box">
-              <Image src="/images/placeholder-box.png" alt="placeholder" width={400} height={200}/>
+              <Image src={thumbnailUrl} alt={`Image of ${file}`} width={400} height={200} unoptimized={true} />
               <p>{file}</p>
               <button>Choose Video</button>
             </div>
-          ))}
+          );
+        })}
       </div>
-        </>
     );
 
 }
